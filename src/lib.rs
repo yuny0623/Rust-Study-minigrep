7b27163aaad519dce1e71b168b427eb304931b65
yuny0623 <overman522@gmail.com>
2022-01-14 14:37:53 +0900

    first rust commit

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+// 에러 때문에 Box<Error> 가 아니라 Box<dyn Error> 로 반환해야함. 
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
